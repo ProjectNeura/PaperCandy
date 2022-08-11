a3ddc6b37c890a65b345fd9fad9eddcc28f68a5a
ATATC <futerry@outlook.com>
2022-08-11 19:03:21 +0800

    Pip synced.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="paper-candy",
-    version="1.0.1b12",
+    version="1.0.2b0",
     author="ATATC",
     author_email="futerry@outlook.com",
     description="A loosely coupled lightweight framework for deep learning papers.",
